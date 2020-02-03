ff44a9c8127f26f56e8c773357d48b735a883ebc
dhyouji <mikkadhiyaulhaq@gmail.com>
2020-02-03 21:42:41 +0700

    Stable

--- app/src/main/java/tif/gaskeun/masodin2/Controller/ordMenuInfo.java
+++ app/src/main/java/tif/gaskeun/masodin2/Controller/ordMenuInfo.java
@@ -1,10 +1,6 @@
 package tif.gaskeun.masodin2.Controller;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,7 +8,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,10 +28,10 @@ public class ordMenuInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ormenu_info);
 
-        assignKey = findViewById(R.id.ordtestkey);
-        namaMn = findViewById(R.id.orddetailNama);
-        deskpMn = findViewById(R.id.orddetailDeskripsi);
-        hargaMn = findViewById(R.id.orddetailHarga);
+        assignKey = findViewById(R.id.testkey);
+        namaMn = findViewById(R.id.detailNama);
+        deskpMn = findViewById(R.id.detailDeskripsi);
+        hargaMn = findViewById(R.id.detailHarga);
 
         String storeKey = getIntent().getStringExtra("storeKey");
         String itemKey = getIntent().getStringExtra("itemKey");
@@ -61,7 +56,7 @@ public class ordMenuInfo extends AppCompatActivity {
             }
         });
 
-        btnBack = findViewById(R.id.btn_back);
+        btnBack = findViewById(R.id.btn_edit);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
