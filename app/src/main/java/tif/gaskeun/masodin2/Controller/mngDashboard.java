00ad4fb75ebc83cc4f2bb1c1d3c64af9d31d302c
dhyouji <mikkadhiyaulhaq@gmail.com>
2020-02-10 14:08:57 +0700

    Stable

--- app/src/main/java/tif/gaskeun/masodin2/Controller/mngDashboard.java
+++ app/src/main/java/tif/gaskeun/masodin2/Controller/mngDashboard.java
@@ -41,7 +41,7 @@ public class mngDashboard extends AppCompatActivity{
     private DatabaseReference dbref,dbref2;
     public Button btn_add,btn_out;
     public RecyclerView rlist, rlist2;
-    public String idtrans,iduser;
+    public String idtrans,iduser,iniid;
     RecyclerView.LayoutManager layoutManager1,layoutManager2;
     FirebaseAuth mAuth;
     String key;
@@ -107,19 +107,19 @@ public class mngDashboard extends AppCompatActivity{
                         }
                     }
 
-                   @Override
-                   public void onCancelled(@NonNull DatabaseError databaseError) {
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                   }
+                    }
 
-               });
-               holder1.itemView.setOnClickListener(new View.OnClickListener() {
-                   @Override
-                   public void onClick(View v) {
-                       String keyX = ((TextView)rlist.findViewHolderForAdapterPosition(position).itemView.findViewById(R.id.list_Key1)).getText().toString();
-                       startActivity(new Intent(getApplicationContext(), mngMenuInfo.class).putExtra("key",keyX));
-                   }
-               });
+                });
+                holder1.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String keyX = ((TextView)rlist.findViewHolderForAdapterPosition(position).itemView.findViewById(R.id.list_Key1)).getText().toString();
+                        startActivity(new Intent(getApplicationContext(), mngMenuInfo.class).putExtra("key",keyX));
+                    }
+                });
             }
 
             @NonNull
@@ -136,19 +136,21 @@ public class mngDashboard extends AppCompatActivity{
         FirebaseRecyclerAdapter<Order2, ListViewHolder5> adapter1 = new FirebaseRecyclerAdapter<Order2, ListViewHolder5>(options1) {
             @Override
             protected void onBindViewHolder(@NonNull final ListViewHolder5 holder2,final int position, @NonNull final Order2 order2) {
-                dbref2.addValueEventListener(new ValueEventListener() {
+                Query query = dbref2;
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for(final DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        for(DataSnapshot snapshot:dataSnapshot.getChildren()){
 
                             idtrans = (snapshot.getKey());
                             holder2.itemName.setText(snapshot.getKey());
-                            dbref2.child(idtrans).addListenerForSingleValueEvent(new ValueEventListener() {
+                            dbref2.child(idtrans);
+                            dbref2.addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    for(DataSnapshot snapshot1:dataSnapshot.getChildren()){
-                                    holder2.itemName.setText(snapshot1.getKey());
-                                    iduser = snapshot1.getKey();
+                                    for (DataSnapshot snapshot1 : dataSnapshot.getChildren()) {
+                                        holder2.itemName.setText(snapshot1.getKey());
+                                        iduser = snapshot1.getKey();
                                     }
                                 }
 
@@ -164,9 +166,8 @@ public class mngDashboard extends AppCompatActivity{
                             query1.addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    for(DataSnapshot snapshot1:dataSnapshot.getChildren()){
-//                                        Order2 ord2 = snapshot1.getValue(Order2.class);
-//                                        holder2.itemName.setText(ord2.getWaktu());
+                                    for(DataSnapshot snapshot2:dataSnapshot.getChildren()){
+                                        iniid = snapshot2.getKey();
                                     }
                                 }
 
@@ -175,6 +176,75 @@ public class mngDashboard extends AppCompatActivity{
 
                                 }
                             });
+
+                            holder2.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Bundle bndle = new Bundle();
+                                    bndle.putString("id1", iduser);
+                                    bndle.putString("id2", iniid);
+                                    Intent intent = new Intent(mngDashboard.this, mngTransInfo.class);
+                                    intent.putExtras(bndle);
+                                    startActivity(intent);
+                                }
+                            });
+//                            FirebaseRecyclerAdapter<Order2, ListViewHolder5> adapter1 = new FirebaseRecyclerAdapter<Order2, ListViewHolder5>(options1) {
+//            @Override
+//            protected void onBindViewHolder(@NonNull final ListViewHolder5 holder2,final int position, @NonNull final Order2 order2) {
+//                dbref2.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+//
+//                            idtrans = (snapshot.getKey());
+//                            holder2.itemName.setText(snapshot.getKey());
+//                            dbref2.child(idtrans);
+//                            dbref2.addListenerForSingleValueEvent(new ValueEventListener() {
+//                                @Override
+//                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                                    for (DataSnapshot snapshot1 : dataSnapshot.getChildren()) {
+//                                        holder2.itemName.setText(snapshot1.getKey());
+//                                        iduser = snapshot1.getKey();
+//                                    }
+//                                }
+//
+//                                @Override
+//                                public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                                }
+//                            });
+//
+////                            iduser = holder2.itemName.getText().toString();
+//
+//                            Query query1 = dbref2.child(idtrans).orderByChild("uid").equalTo(iduser);
+//                            query1.addListenerForSingleValueEvent(new ValueEventListener() {
+//                                @Override
+//                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                                    for(DataSnapshot snapshot2:dataSnapshot.getChildren()){
+//                                        iniid = snapshot2.getKey();
+////                                        Order2 ord = snapshot2.getValue(Order2.class);
+////                                        Order2 ord2 = snapshot1.getValue(Order2.class);
+////                                        holder2.itemName.setText(ord2.getWaktu());
+//                                    }
+//                                }
+//
+//                                @Override
+//                                public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                                }
+//                            });
+//
+//                            holder2.itemView.setOnClickListener(new View.OnClickListener() {
+//                                @Override
+//                                public void onClick(View v) {
+//                                    Bundle bndle = new Bundle();
+//                                    bndle.putString("id1", iduser);
+//                                    bndle.putString("id2", iniid);
+//                                    Intent intent = new Intent(mngDashboard.this, mngTransInfo.class);
+//                                    intent.putExtras(bndle);
+//                                    startActivity(intent);
+//                                }
+//                            });
                         }
                     }
 
---
+++ app/src/main/java/tif/gaskeun/masodin2/Controller/mngTransInfo.java
@@ -0,0 +1,134 @@
+package tif.gaskeun.masodin2.Controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import tif.gaskeun.masodin2.Iface.ListViewHolder4;
+import tif.gaskeun.masodin2.Model.Informasi;
+import tif.gaskeun.masodin2.Model.Order2;
+import tif.gaskeun.masodin2.R;
+
+public class mngTransInfo extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    TextView nma, nmja, wktu, cnct, rest, addrs, total;
+    public DatabaseReference dbref, dbref2;
+    RecyclerView rlist;
+    int calctotal;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trans_info);
+
+        mAuth = FirebaseAuth.getInstance();
+        nma = findViewById(R.id.tv_nma);
+        nmja = findViewById(R.id.tv_nomej);
+        wktu = findViewById(R.id.tv_wktu);
+        cnct = findViewById(R.id.tv_cnct);
+        rest = findViewById(R.id.tv_restname);
+        addrs = findViewById(R.id.tv_restaddrs);
+        rlist = findViewById(R.id.rlistTrans);
+        total = findViewById(R.id.tv_totalharga);
+
+        String key = mAuth.getUid();
+        final String key1 = getIntent().getStringExtra("id1");
+        final String key2 = getIntent().getStringExtra("id2");
+
+        dbref = FirebaseDatabase.getInstance().getReference(key);
+        dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    Informasi info = dataSnapshot.getValue(Informasi.class);
+                    String nrest = info.getRestoran();
+                    String addrsr = info.getAlamat();
+                    rest.setText(nrest);
+                    addrs.setText(addrsr);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        dbref = FirebaseDatabase.getInstance().getReference(key).child("Transaksi");
+        dbref.child(key1).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Order2 ord = dataSnapshot.getValue(Order2.class);
+                    nma.setText("Nama: "+ord.getNama());
+                    nmja.setText("No. Meja: "+ord.getNmeja());
+                    wktu.setText("Waktu: "+ord.getWaktu());
+                    cnct.setText("Kontak: "+ord.getKontak());
+                }
+                ;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        calctotal = 0;
+        rlist.setHasFixedSize(false);
+        layoutManager = new LinearLayoutManager(this);
+        rlist.setLayoutManager(layoutManager);
+        dbref2 = FirebaseDatabase.getInstance().getReference(key);
+        FirebaseRecyclerOptions<Order2> options = new FirebaseRecyclerOptions.Builder<Order2>().setQuery(dbref2.child("Transaksi").child(key1).child("Menu"), Order2.class).build();
+        FirebaseRecyclerAdapter<Order2, ListViewHolder4> adapter = new FirebaseRecyclerAdapter<Order2, ListViewHolder4>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ListViewHolder4 holder4, int position, @NonNull Order2 order2) {
+                String Menu = order2.getMnNama();
+                String Harga = order2.getMnHarga();
+                String Qty = order2.getMnQty();
+
+                holder4.itemName.setText(Menu);
+                holder4.itemPrice.setText("Rp. "+Harga);
+                holder4.itemQty.setText("("+Qty+")");
+
+                int Total = Integer.parseInt(Harga) * Integer.parseInt(Qty);
+                calctotal = calctotal + Total;
+                String Totalfx = Integer.toString(Total);
+
+                holder4.itemTotal.setText("Rp. "+Totalfx);
+                total.setText("Rp. "+Integer.toString(calctotal));
+            }
+
+            @NonNull
+            @Override
+            public ListViewHolder4 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item4, parent, false);
+                ListViewHolder4 holder4 = new ListViewHolder4(view);
+                return holder4;
+            }
+        };
+
+        rlist.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+}
